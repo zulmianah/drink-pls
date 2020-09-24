54265fa0c4ba96fb6a5094c762a96ae8288bf29b
Zulmianah <40556052+zulmianah@users.noreply.github.com>
2020-09-24 09:07:08 +0300

    move left right

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -41,10 +41,11 @@ public class PlayerController : MonoBehaviour
         moveInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
 
-        if (facingRight==false&&moveInput>0)
+        if (facingRight==true&&moveInput>0)
         {
             Flip();
-        }else if(facingRight == true && moveInput < 0)
+        }
+        else if(facingRight == false && moveInput < 0)
         {
             Flip();
         }
